a8491d22381704b22b4814542e66030ace872385
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-05 01:07:28 +0530

    Update db_connect.php

--- faculty/db_connect.php
+++ faculty/db_connect.php
@@ -1,13 +1,12 @@
 <?php
-    $host = '{{DB_HOST}}';
-    $db = '{{DB_NAME}}';
-    $user = '{{DB_USER}}';
-    $pass = '{{DB_PASS}}';
+$host = '{{DB_HOST}}';
+$db = '{{DB_NAME}}';
+$user = '{{DB_USER}}';
+$pass = '{{DB_PASS}}';
 
-    $conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
