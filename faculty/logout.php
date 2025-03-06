6e12644b1dc51188e4506500c2a8e35be92634f9
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-06 15:24:41 +0530

    Update logout.php

--- faculty/logout.php
+++ faculty/logout.php
@@ -15,6 +15,6 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 // Redirect to the login page
-header("Location: faculty_login.php");
+header("Location: ../index.php");
 exit;
 ?>
