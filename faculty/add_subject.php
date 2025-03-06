2bba9100e63462456acf2ac111dd0396912025ec
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-06 15:26:06 +0530

    Update add_subject.php

--- faculty/add_subject.php
+++ faculty/add_subject.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+// Prevent caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 // Check if the user is logged in
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
