652aefa6eef58da962cb55573769282a05ab7fc1
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-06 15:36:05 +0530

    Update add_grades.php

--- faculty/add_grades.php
+++ faculty/add_grades.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-// Prevent caching
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+
 
 // Handle form submission
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
