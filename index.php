ce5907e95a824142da1b67ead6240216516e9f89
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-11 23:56:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Login Page</title>
+    <title>CAHCET - Student Management System</title>
     <style>
         /* Existing styles remain unchanged */
         body {
