b5dc2c6460c42d4eb3371a596d8b7167a69ab937
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-14 06:50:58 +0530

    Update generate_marksheet.php

--- faculty/generate_marksheet.php
+++ faculty/generate_marksheet.php
@@ -1,8 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-$show_alert = false; // Flag to control alert display
 session_start();
 
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
