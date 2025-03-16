98857df02d0a9f738c2f24bffe194c3ea727ab80
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-16 08:48:26 +0530

    Update generate_pdf.php

--- faculty/generate_pdf.php
+++ faculty/generate_pdf.php
@@ -94,7 +94,7 @@ $pdf->SetFont('Times', '', 12); // Regular for value
 $pdf->Cell(70, 10, $student['name'], 0, 0, 'L'); // Value
 
 $pdf->SetFont('Times', 'B', 12); // Bold for labels
-$pdf->Cell(70, 10, 'Roll No.: ', 0, 0, 'R'); // Label (reduced width)
+$pdf->Cell(80, 10, 'Roll No.: ', 0, 0, 'R'); // Label (reduced width)
 $pdf->SetFont('Times', '', 12); // Regular for value
 $pdf->Cell(16, 10, $student['roll_no'], 0, 1, 'R'); // Value
 
@@ -105,7 +105,7 @@ $pdf->Cell(70, 10, $year, 0, 0, 'L'); // Value
 
 // Set font for student info (labels in bold, values in regular)
 $pdf->SetFont('Times', 'B', 12); // Bold for labels
-$pdf->Cell(70, 10, 'Branch & Section: ', 0, 0, 'R'); // Label aligned to the right
+$pdf->Cell(80, 10, 'Branch & Section: ', 0, 0, 'R'); // Label aligned to the right
 $pdf->SetFont('Times', '', 12); // Regular for value
 $pdf->Cell(16, 10, $branch . ' & ' . $section, 0, 1, 'R'); // Value aligned to the right
 
