f251f880e9f84dcdd7d23000ad8fdcde04822107
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-16 08:00:43 +0530

    Update generate_pdf.php

--- faculty/generate_pdf.php
+++ faculty/generate_pdf.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Include the FPDF library
 require('../fpdf186/fpdf.php'); // Update the path to where you extracted FPDF
 
