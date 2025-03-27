be0c90bd8ab7330d8e9c8805fa618620a33b3ec0
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-03-28 05:17:22 +0530

    Update generate.php

--- faculty/generate.php
+++ faculty/generate.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header('Location: ../index.php');
     exit;
