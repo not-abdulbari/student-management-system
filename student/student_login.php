299355cf537ec2bb2bb6304a4371a0c1fe5adecc
not-abdulbari <146872928+not-abdulbari@users.noreply.github.com>
2025-04-22 01:40:44 +0530

    Update student_login.php

--- student/student_login.php
+++ student/student_login.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+
 session_start();
 if (!isset($_SESSION['student_logged_in']) || !isset($_SESSION['roll_no'])) {
     header('Location: ../index.php');
